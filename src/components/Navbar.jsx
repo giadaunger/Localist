df8d9db34c00f3af11a7e7e16c7d0887f003f0db
Giada Unger <giada.unger02@gmail.com>
2026-01-03 18:32:36 +0100

    navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router'
+import logo from '../assets/logo.png'
 
 function Navbar() {
   return (
-    <div className='bg-red-100'>Navbar</div>
+    <div className="bg-[#FDFBF7] p-4 border-b border-[#E8E4DE] shadow-md">
+      <div className="w-4/5 mx-auto justify-between flex flex-row items-center">
+        <NavLink to="/">
+          <img 
+            src={logo} 
+            alt="logo" 
+            className="w-28 h-20"
+          />
+        </NavLink>
+        <button className="py-2 px-4 text-md font-semibold text-white rounded-full bg-[#2C3E50] hover:bg-black ransform transition duration-300 hover:scale-105">Join the next edition</button>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-}
+    content: [
+      './index.html',         
+      './src/**/*.{js,ts,jsx,tsx}',  // This should cover all your source files
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+            belleza: ["Belleza", "sans-serif"]
+        },
+      },
+    },
+    plugins: [],
+  };
