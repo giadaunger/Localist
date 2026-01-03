3984df20fd57bdf7d3dd75d52a47906e90a89280
Giada Unger <giada.unger02@gmail.com>
2026-01-03 16:40:43 +0100

    setup

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
